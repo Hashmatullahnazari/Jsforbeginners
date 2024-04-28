edaf0fbbca92f015dfa2e1533e1334e105e015f3
Hash Nazari <Hash@hashmatullahnazari.local>
2024-04-28 16:51:13 -0500

    Initial commit

---
+++ logic.js
@@ -0,0 +1,192 @@
+// Initialize a boolean variable 'laundryDone' and set it to false
+// let laundryDone = false;
+
+// Check the value of 'laundryDone'
+
+// if (laundryDone) {
+//     If 'laundryDone' is true, print the message "Great, the laundry is all done"
+//     console.log("Great, the laundry is all done");
+
+// } else {
+//     If 'laundryDone' is false, print the message "Time to start the Laundry"
+//     console.log("Time to start the Laundry");
+// }
+
+
+
+
+// let laundryDone = true;
+
+// if (laundryDone) {
+//     console.log("Great, the laundry is all done");
+// } else {
+//     console.log("Time to start Laundry");
+// }
+
+
+// let dishesDone = false;
+
+// if(dishesDone){
+// console.log("The dishes are clean and put away. Nice work!");
+// }else{
+// console.log("The sink is full of dirty dishes. Time to get started!");
+// }
+
+
+// let dishesDone = true;
+
+// if(dishesDone){
+// console.log("The dishes are clean and put away. Nice work!");
+// }else{
+// console.log("The sink is full of dirty dishes. Time to get started!");
+// }
+
+
+// let vacuumed = false;
+
+// if (vacuumed) {
+//     console.log("The floor looks great after vacuuming!")
+// } else {
+//     console.log("The carpets need a good vacuuming.Get to it!")
+// }
+
+// let vacuumed = true;
+
+// if (vacuumed) {
+//     console.log("The floor looks great after vacuuming!");
+// } else {
+//     console.log('The carpets need a good vacuuming. Get to it!');
+// }
+
+
+// Initialize a boolean variable 'dusted' and set it to false
+// let dusted = false;
+
+// Check the value of 'dusted'
+// if (dusted) {
+//     If 'dusted' is true, print the message "The surfaces are dust-free, nice and clean!"
+//     console.log("The surfaces are dust-free, nice and clean!");
+// } else {
+//     If 'dusted' is false, print the message "Time to grab the duster and tackle those dusty surfaces."
+//     console.log("Time to grab the duster and tackle those dusty surfaces.");
+// }
+
+
+// Initialize a boolean variable 'dusted' and set it to true
+// let dusted = true;
+
+// Check the value of 'dusted'
+// if (dusted) {
+//     If 'dusted' is true, print the message "The surfaces are dust-free, nice and clean!"
+//     console.log("The surfaces are dust-free, nice and clean");
+
+// }else {
+//     If 'dusted' is false, print the message "Time to grab the duster and tackle those dusty surfaces."
+// console.log("Time to grab the duster and tackle those dusty surfaces.");
+
+// }
+
+
+// Initialize a boolean variable 'floorsCleaned' and set it to false
+// let floorsCleaned = false;
+//
+// Check the value of 'floorsCleaned'
+// if (floorsCleaned) {
+//
+//    If 'floorsCleaned' is true, print the message "The floors are sparkling clean"
+//    console.log("The floors are sparkling clean");
+//} else {
+//    If 'floorsCleaned' is false, print the message "The floors need a good sweeping and mopping. Get to it"
+//     console.log("The floors need a good sweeping and mopping. Get to it")
+//}
+
+
+// let floorsCleaned = true;
+// if (floorsCleaned) {
+//     console.log("The floors are sparkling clean!");
+// } else {
+//     console.log("The floors need a good sweeping. Get to it!");
+// }
+
+
+// let trashTaken = false;
+
+// if (trashTaken){
+//     console.log("The trash has been taken out, great job!")
+// }else{
+//     console.log("The trash is piling up, time to take it out");
+// }
+
+
+// let trashTaken = true;
+
+// if (trashTaken){
+//     console.log("The trash has been taken out, great job!");
+// }else{
+//     console.log("The trash is pilling up, time to take it out")
+// }
+
+
+// let bathroomClean = false;
+
+// if (bathroomClean){
+//     console.log("The bathroom is spotless, nicely done!")
+// }else{
+//     console.log("The bathroom needs a good cleaning, get to it!")
+// }
+
+
+// let bathroomClean = true;
+
+// if (bathroomClean){
+//     console.log("The bathroom is spotless, nicely done!")
+// }else{
+//     console.log("The bathroom needs a good cleaning, get to it!");
+// }
+
+
+// let plantsWatered = false;
+
+// if (plantsWatered) {
+//     console.log("The plants are all hydrated and looking healthy!");
+// } else{
+//     console.log("The plants needs to be watered, don't forget");
+
+// }
+
+
+// let plantsWatered = true;
+
+// if (plantsWatered) {
+//     console.log("The plants are all hydrated and looking healthy!");
+// } else {
+//     console.log("The plants needs to be watered, don't forget!")
+// }
+
+
+
+// let closetOrganized = false;
+
+// if (closetOrganized) {
+//     console.log("The closet is neat and tidy, great job!");
+// } else {
+//     console.log("Time to tackle that messy closet!");
+// }
+
+
+// let closetOrganized = true;
+
+// if (closetOrganized) {
+//     console.log("The closet is neat and tidy, great job!");
+// } else {
+//     console.log("Time to tackle the messy closet!");
+// }
+
+// let windowsCleaned = false;
+
+// if (windowsCleaned) {
+//     console.log("The windows are sparkling clean, nicely done!");
+// } else {
+//     console.log ("The windows need a good cleaning, get to it!")
+// }
+
