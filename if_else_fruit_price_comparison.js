3f5aed01cee7cb0ab9f0ac6c1c314109e64e4520
hashmatullah nazari <hn0153@gmail.com>
2024-05-02 16:17:01 -0500

    ifelsefruitpricecomparisonjs

--- if_else_fruit_price_comparison.js
+++ if_else_fruit_price_comparison.js
@@ -10,13 +10,35 @@
 // }
 
 
-let strawberryPrice = 4.99;
-let blueberryPrice = 3.75;
+// let strawberryPrice = 4.99;
+// let blueberryPrice = 3.75;
 
-if (strawberryPrice > blueberryPrice) {
-    console.log("Strawberries are more expensive than blueberries.");
-} else if (blueberryPrice < strawberryPrice) {
-    console.log("Blueberries are more expensive than strawberries");
-} else {
-    console.log("Strawberries and blueberries have the same price..");
-}
+// if (strawberryPrice > blueberryPrice) {
+//     console.log("Strawberries are more expensive than blueberries.");
+// } else if (blueberryPrice < strawberryPrice) {
+//     console.log("Blueberries are more expensive than strawberries.");
+// } else {
+//     console.log("Strawberries and blueberries have the same price.");
+// }
+
+// let pineapplePrice = 5.50; 
+// let mangoPrice = 2.99;
+
+// if (pineapplePrice > mangoPrice) {
+//     console.log("Pineapples are more expensive than mangoes.");
+// } else if (pineapplePrice < mangoPrice) {
+//     console.log("Manoges are more expensive than pineapples");
+// } else {
+//     console.log("Pineapples and mangoes have the same price.");
+// }
+
+// let kiwiPrice = 1.25;
+// let papayaPrice = 3.75;
+
+// if (kiwiPrice > papayaPrice) {
+//     console.log("kiwis ae more expensive than papayas")
+// } else if (kiwiPrice < papayaPrice) {
+//     console.log("papayas are more expensive thank kiwis");
+// } else {
+//     console.log("kiwis and papayas have the same price.");
+// } 
