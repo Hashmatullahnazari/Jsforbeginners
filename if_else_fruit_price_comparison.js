fe5ffb6099a4324ef093a20ad7f4e6a7b94883b9
hashmatullah nazari <hn0153@gmail.com>
2024-05-02 10:19:56 -0500

    ifelsefruitpricecomparison

---
+++ if_else_fruit_price_comparison.js
@@ -0,0 +1,22 @@
+// let orangePrice = 3.25;
+// let grapefruitPrice = 2.50;
+
+// if (orangePrice > grapefruitPrice) {
+//     console.log("Oranges are more expensive than grapefruits");
+// } else if (orangePrice < grapefruitPrice){
+//     console.log("Grapefruits are more expensive then oranges.");
+// } else {
+//     console.log("Oranges and grapfruits have the same price.");
+// }
+
+
+let strawberryPrice = 4.99;
+let blueberryPrice = 3.75;
+
+if (strawberryPrice > blueberryPrice) {
+    console.log("Strawberries are more expensive than blueberries.");
+} else if (blueberryPrice < strawberryPrice) {
+    console.log("Blueberries are more expensive than strawberries");
+} else {
+    console.log("Strawberries and blueberries have the same price..");
+}
