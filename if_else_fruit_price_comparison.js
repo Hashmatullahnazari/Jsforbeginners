00c018ff1d6a00f474a37e6ecfee40f848f828e8
Hashmatullah Nazari <hn0153@gmail.com>
2024-05-03 09:41:19 -0500

    if_else_fruit_price_comparison.js

--- if_else_fruit_price_comparison.js
+++ if_else_fruit_price_comparison.js
@@ -1,44 +1,113 @@
-// let orangePrice = 3.25;
-// let grapefruitPrice = 2.50;
+/*
+// Set the price of oranges to $3.25
+let orangePrice = 3.25;
 
-// if (orangePrice > grapefruitPrice) {
-//     console.log("Oranges are more expensive than grapefruits");
-// } else if (orangePrice < grapefruitPrice){
-//     console.log("Grapefruits are more expensive then oranges.");
-// } else {
-//     console.log("Oranges and grapfruits have the same price.");
-// }
+// Set the price of grapefruits to $2.50
+let grapefruitPrice = 2.50;
+
+// Check if the price of oranges is greater than the price of grapefruits
+// The parentheses () group the comparison expression
+// The greater than > operator compares the values of orangePrice and grapefruitPrice
+if (orangePrice > grapefruitPrice) {
+    // If oranges are more expensive, log this message
+    console.log("Oranges are more expensive than grapefruits.");
+} 
+// If the above condition is false, check if the price of oranges is less than the price of grapefruits
+// The parentheses () group the comparison expression
+// The less than < operator compares the values of orangePrice and grapefruitPrice
+else if (orangePrice < grapefruitPrice){
+    // If grapefruits are more expensive, log this message
+    console.log("Grapefruits are more expensive than oranges.");
+} 
+// If neither of the above conditions are true, it means the prices are the same
+else {
+    // Log this message
+    console.log("Oranges and grapefruits have the same price.");
+}
+
+*/
 
 
-// let strawberryPrice = 4.99;
-// let blueberryPrice = 3.75;
+/*
 
-// if (strawberryPrice > blueberryPrice) {
-//     console.log("Strawberries are more expensive than blueberries.");
-// } else if (blueberryPrice < strawberryPrice) {
-//     console.log("Blueberries are more expensive than strawberries.");
-// } else {
-//     console.log("Strawberries and blueberries have the same price.");
-// }
+let strawberryPrice = 4.99;
+let blueberryPrice = 3.75;
 
-// let pineapplePrice = 5.50; 
-// let mangoPrice = 2.99;
+ if (strawberryPrice > blueberryPrice) {
+     console.log("Strawberries are more expensive than blueberries.");
+ } else if (blueberryPrice < strawberryPrice) {
+     console.log("Blueberries are more expensive than strawberries.");
+ } else {
+     console.log("Strawberries and blueberries have the same price.");
+ }
 
-// if (pineapplePrice > mangoPrice) {
-//     console.log("Pineapples are more expensive than mangoes.");
-// } else if (pineapplePrice < mangoPrice) {
-//     console.log("Manoges are more expensive than pineapples");
-// } else {
-//     console.log("Pineapples and mangoes have the same price.");
-// }
+ */
 
-// let kiwiPrice = 1.25;
-// let papayaPrice = 3.75;
 
-// if (kiwiPrice > papayaPrice) {
-//     console.log("kiwis ae more expensive than papayas")
-// } else if (kiwiPrice < papayaPrice) {
-//     console.log("papayas are more expensive thank kiwis");
-// } else {
-//     console.log("kiwis and papayas have the same price.");
-// } 
+
+ /*
+ let pineapplePrice = 5.50; 
+ let mangoPrice = 2.99
+ if (pineapplePrice > mangoPrice) {
+     console.log("Pineapples are more expensive than mangoes.");
+ } else if (pineapplePrice < mangoPrice) {
+     console.log("Manoges are more expensive than pineapples");
+ } else {
+     console.log("Pineapples and mangoes have the same price.");
+ }
+
+ */
+
+ /*
+ let kiwiPrice = 1.25;
+ let papayaPrice = 3.75
+ if (kiwiPrice > papayaPrice) {
+     console.log("kiwis ae more expensive than papayas")
+ } else if (kiwiPrice < papayaPrice) {
+     console.log("papayas are more expensive thank kiwis");
+ } else {
+     console.log("kiwis and papayas have the same price.");
+ } 
+
+  */
+
+ /*
+
+ let peachPrice = 2.75;
+ let nectarinePrice = 3.25
+ if (peachPrice > nectarinePrice) {
+     console.log("Peaches are more expansive thank nectarines");
+ } else if (peachPrice < nectarinePrice) {
+     console.log("Nectarines are more expansive than peaches");
+ } else {
+     console.log("Peaches and nectarines have the same price.");
+ }
+ */
+
+ /*
+// Set the price of plums to $2.50
+let plumPrice = 2.50;
+
+// Set the price of cherries to $4.99
+let cherryPrice = 4.99;
+
+// Check if the price of plums is greater than the price of cherries
+// The parentheses () group the comparison expression
+// The greater than > operator compares the values of plumPrice and cherryPrice
+if (plumPrice > cherryPrice) {
+    // If plums are more expensive, log this message
+    console.log("Plums are more expensive than cherries.");
+} 
+// If the above condition is false, check if the price of plums is less than the price of cherries
+// The parentheses () group the comparison expression
+// The less than < operator compares the values of plumPrice and cherryPrice
+else if (plumPrice < cherryPrice) {
+    // If cherries are more expensive, log this message
+    console.log("Cherries are more expensive than plums.");
+} 
+// If neither of the above conditions are true, it means the prices are the same
+else {
+    // Log this message
+    console.log("Plums and cherries have the same price.");
+}
+ */
