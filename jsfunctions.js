382bb05cad148c107d49a71fcfbfca20d660ed71
Hashmatullah Nazari <hn0153@gmail.com>
2024-11-18 12:58:57 -0600

    Update jsfunctions.js

--- jsfunctions.js
+++ jsfunctions.js
@@ -21,3 +21,27 @@ if (oilChanged){
 
 console.log(checkCarMaintenaceStatus(false));
 console.log(checkCarMaintenaceStatus(true));
+
+
+function checkLaundryStatus(laundryDone){
+  if(laundryDone) {
+    return"All clothes are clean and folded. Great job!";
+  } else 
+  {
+    return"There's laundry waiting to be done. Time to wash!"
+  }
+}
+console.log(checkLaundryStatus(false));
+console.log(checkLaundryStatus(true));
+
+
+
+function checkEmailStatus(inboxCleared){
+  if(inboxCleared){
+    return"Your inbox is all caught up. Well done!";
+  } else {
+    return"You have unread emails. Time to check your inbox!";
+  }
+}
+console.log(checkEmailStatus(false));
+console.log(checkEmailStatus(true));
